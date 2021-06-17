bb163a36ba2d65b966fc462cec6d7ffdd0796eed
Steve Gardner <github@ste.vg>
2021-06-17 20:23:15 +0100

    link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
     return (
     <div className="App">
       <BurgerDrum />
+      <a href="https://youtu.be/VtmsuVT7BGI" target="_blank" className="component-carousel">
+        <span>Watch us make this on</span>
+        <img src="/images/component-carousel.png" alt="Component Carousel" /> 
+      </a>
     </div>
   );
 }
